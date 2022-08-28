b9fde4f4a1298a22d9fe883d01e55593c601089b
TungHV-Dev <havantung10d@gmail.com>
2022-08-28 17:09:10 +0700

    add server configuration

--- server.js
+++ server.js
@@ -3,4 +3,10 @@ const app = express()
 
 global._config = require('./utils/config')
 
-console.log(_config.SERVER.HOST)
+const port = _config.SERVER.PORT
+
+app.use(express.json())
+
+app.listen(port, () => {
+    console.log(`Server is running at port: ${port}`)
+})
