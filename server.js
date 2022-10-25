10c7f20d06309f17629fe4fdb6e4239a36c19017
TungHV-Dev <havantung10d@gmail.com>
2022-10-25 14:33:52 +0700

    remove test file

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
+const test = require('./test')
 
 global._config = require('./utils/config')
 
 const port = _config.SERVER.PORT
 
 app.use(express.json())
+
+app.get('/exams', (req, res) => {
+    test.generate_new_exams(req, res)
+})
 
 app.listen(port, () => {
     console.log(`Server is running at port: ${port}`)
