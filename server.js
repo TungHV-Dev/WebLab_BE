52617803cb6d997a11ae8eb23d4b55cd651f2043
TungHV-Dev <havantung10d@gmail.com>
2022-12-07 03:04:56 +0700

    add storage to firebase

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ global._config = require(`./configs/${NODE_ENV}`)
 
 const firebaseConnection = require('./connections/firebase')
 global._database = firebaseConnection.createDatabase()
+global._storage = firebaseConnection.createStorage()
 
 const bodyParser = require('body-parser')
 const port = NODE_ENV == 'uat' ? process.env.PORT : _config.SERVER.PORT
